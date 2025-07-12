43cbc2228c02f8e2d3cf8883f467a9efdf81a5f0
AnasIqbaI <arafdarky@gmail.com>
2025-07-12 23:21:20 +0600

    Introduction to Python

---
+++ New.py
@@ -0,0 +1,5 @@
+import keyword
+
+print("Welcome to world of Programming!")
+
+print("Python Keywords:" , keyword.kwlist)
---
+++ intro.py
@@ -0,0 +1,14 @@
+#This is a Introduction to Python script 
+print("Hello World!")
+
+#This is printing numbers
+print(1)
+
+#This is printing a sum
+print(2+3)
+
+#This is a comment explaining the next line
+print("2+3")
+
+#Printing and performing a calculation
+print("The result of 2+3 is:" , 2 + 3)
